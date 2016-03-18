7817e8211e51ca4fce5cddbd016e85e1940da75a
chrishey <chrishey87@hotmail.com>
2016-03-18 15:08:50 +0000

    Loading data from a csv and displaying it in the interactive window

--- src/project.rattlesnake.data/Script.fsx
+++ src/project.rattlesnake.data/Script.fsx
@@ -1,8 +1,23 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
+﻿#load "Library1.fs"
 open project.rattlesnake.data
+open System.IO
+open System
 
-// Define your library scripting code here
+type Observation = { Date:DateTime; HomeTeam: string; AwayTeam: string; HomeGoals: int; AwayGoals: int}
 
+let toObservation (csvData:string) = 
+    let columns = csvData.Split(',')
+    let date = columns.[0]
+    let homeTeam = columns.[1]
+    let awayTeam = columns.[2]
+    let homeGoals = columns.[3]
+    let awayGoals = columns.[4]
+    {Date=DateTime.Parse(date); HomeTeam=homeTeam; AwayTeam=awayTeam; HomeGoals=Convert.ToInt32(homeGoals); AwayGoals=Convert.ToInt32(awayGoals);}
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"C:\Users\Chris Hey\Documents\Projects\Git\project-rattlesnake\data\TestSample.csv"
+let trainingData = reader trainingPath
